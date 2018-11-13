90d7b84640233c0380e15c3b663cd980754a10f8
Nur Ainun <44445260+Xagung@users.noreply.github.com>
2018-11-13 18:33:38 +0700

    Kelas XATool tambah fitur
    Fitur capWord.

--- app/src/main/java/id/kaliagung/translator/XATool.java
+++ app/src/main/java/id/kaliagung/translator/XATool.java
@@ -4,8 +4,19 @@ public class XATool{
 	public String change(String a, String b, String c){
 		g = new StringBuilder();
 		g.append(a);
-		while (g.indexOf(b) > -1) g.replace(g.indexOf(b), g.indexOf(b) + 2, c);
+		while (g.indexOf(b) > -1) g.replace(g.indexOf(b), g.indexOf(b) + b.length(), c);
 		return g.toString();
+	}
+	public String capWord(String a){
+		g = new StringBuilder();
+		if(a.length() > 1){
+			g.append(cap(a));
+			for(int i = 0; i < g.length() - 2; i++){
+				if(" ".indexOf(g.charAt(i)) > -1)
+					g.replace(i + 1, i + 2, g.substring(i + 1, i + 2).toUpperCase());
+			}
+			return g.toString();
+		}else return cap(a);
 	}
 	public String cap(String a){
 		if(a.length() >= 1) return a.toUpperCase().charAt(0)+subAfter(a.toLowerCase(), 1);
